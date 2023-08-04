7a669d73d81e8bbb004d79c7bba41616e198bc0a
acakebread <acakebread@gmail.com>
2023-08-04 09:43:10 +0100

    added some models and old eggbot
    new eggbot needed

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2019 massivehadron.com ltd. created 11/07/2019 by Andrew Cakebread
+
+using UnityEngine;
+
+namespace Decology
+{
+	public class CameraMovement : MonoBehaviour
+	{
+		public float lookSpeedH = 2f;
+		public float lookSpeedV = 2f;
+		public float zoomSpeed = 2f;
+		public float dragSpeed = 6f;
+
+		private float yaw = 0f;
+		private float pitch = 0f;
+
+		private void Start()
+		{
+			yaw = transform.eulerAngles.y;
+			pitch = transform.eulerAngles.x;
+		}
+
+		void Update()
+		{
+			float pointer_x = Input.GetAxis("Mouse X");
+			float pointer_y = Input.GetAxis("Mouse Y");
+			if (Input.touchCount > 0)
+			{
+				pointer_x = Input.touches[0].deltaPosition.x * 0.05f;
+				pointer_y = Input.touches[0].deltaPosition.y * 0.05f;
+			}
+
+			if (Input.touchCount > 0 || Input.GetMouseButton(0) || Input.GetMouseButton(1))
+			{
+				yaw += lookSpeedH * pointer_x;
+				pitch -= lookSpeedV * pointer_y;
+
+				transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+			}
+
+			//drag camera around with Middle Mouse
+			if (Input.GetMouseButton(2))
+			{
+				transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
+			}
+
+			//Zoom in and out with Mouse Wheel
+			transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
+		}
+	}
+}
---
+++ Assets/Scripts/PreviewCamera.cs
@@ -0,0 +1,71 @@
+﻿// Copyright 2018 massivehadron.com ltd. created 04/04/2018 by Andrew Cakebread
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Decology
+{
+	public class PreviewCamera : MonoBehaviour
+	{
+		//test camera 
+		private GameObject camera_pivot
+		{
+			get
+			{
+				if (null == transform.Find("pivot"))
+				{
+					GameObject pivot = new GameObject("pivot");
+					pivot.transform.SetParent(transform.parent, false);
+					pivot.transform.rotation = transform.rotation;
+					pivot.transform.SetParent(transform);
+				}
+				return transform.Find("pivot").gameObject;
+			}
+		}
+
+		private Vector3 pivotAngle = Vector3.zero;
+		private Vector3 rotation = new Vector3(0, 0, 0);
+
+		private void ApplyDelta(Transform pivot,Vector3 delta)
+		{
+			pivotAngle += delta;
+			pivotAngle.x = Mathf.Clamp(pivotAngle.x, -50, 50);
+			pivotAngle.y = Mathf.Clamp(pivotAngle.y + rotation.y, -38, 0) - rotation.y;
+
+			Vector3 combined = rotation + pivotAngle;
+			pivot.rotation = new Quaternion();
+			pivot.Rotate(0, combined.x, 0);
+			pivot.Rotate(-combined.y, 0, 0);
+		}
+
+		private void Awake()
+		{
+			Vector3 euler = camera_pivot.transform.eulerAngles;
+			rotation = new Vector3(normalizeAngle(euler.y), normalizeAngle(-euler.x), 0);
+		}
+
+		private Vector3 old = Vector3.zero;
+		private void Update()
+		{
+			if (Input.GetMouseButton(0))
+			{
+				Transform pivot = camera_pivot.transform;
+				pivot.SetParent(transform.parent);
+				transform.SetParent(pivot);
+				ApplyDelta(pivot, (Input.mousePosition - old) * 0.2f);
+				transform.SetParent(pivot.parent);
+				pivot.SetParent(transform);
+			}
+			old = Input.mousePosition;
+		}
+
+		//utilities
+		private static float UMODF(float val, float mod) { return (val % mod + mod) % mod; }//positive modulo
+
+		private static float normalizeAngle(float angle)
+		{
+			return UMODF(angle + 180, 360) - 180;
+		}
+	}
+}
