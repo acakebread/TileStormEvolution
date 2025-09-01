279f267cb973cb88b8672ef4d713ce3459f84cdf
acakebread <acakebread@gmail.com>
2025-09-01 23:31:33 +0100

    stable version of json generator

--- Assets/System/Tests/jsonExample/JsonDeserialisationExample.cs
+++ Assets/System/Tests/jsonExample/JsonDeserialisationExample.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using System.IO.Pipes;
 
 public class JsonDeserializationExample : MonoBehaviour
 {
@@ -24,7 +23,9 @@ public class JsonDeserializationExample : MonoBehaviour
       ]
     }
   ]
-}";
+}
+
+";
 		try
 		{
 			var data = JsonTocs.FromJson<Dictionary<string, object>>(jsonString);
@@ -38,11 +39,60 @@ public class JsonDeserializationExample : MonoBehaviour
 			{
 			  _type = (object)null,
 			  DocumentIndex = (object)null,
+			  Name = (object)null,
 			  Title = (object)null
 			} }
 		  } }
 				};
 
+				var result = new
+				{
+					Groups = (data.ContainsKey("Groups") ? ((object[])data["Groups"]).Select((item, j) =>
+					{
+						var subDict = (Dictionary<string, object>)item;
+						return new
+						{
+							Items = subDict.ContainsKey("Items") ? ((object[])subDict["Items"]).Select((innerItem, k) =>
+							{
+								var innerDict = (Dictionary<string, object>)innerItem;
+								return new
+								{
+									_type = innerDict.ContainsKey("$type") ? innerDict["$type"] : (object)null,
+									DocumentIndex = innerDict.ContainsKey("DocumentIndex") ? innerDict["DocumentIndex"] : (object)null,
+									Name = innerDict.ContainsKey("Name") ? innerDict["Name"] : (object)null,
+									Title = innerDict.ContainsKey("Title") ? innerDict["Title"] : (object)null
+								};
+							}).ToArray() : new[] {                   new
+				  {
+					_type = (object)null,
+					DocumentIndex = (object)null,
+					Name = (object)null,
+					Title = (object)null
+				  } }
+						};
+					}).ToArray() : new[] {               new
+			  {
+				Items = new[] {                 new
+				{
+				  _type = (object)null,
+				  DocumentIndex = (object)null,
+				  Name = (object)null,
+				  Title = (object)null
+				} }
+			  } })
+				};
+
+				// Display deserialized values
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 0 && result.Groups[0].Items[0]._type != null) Debug.Log($"Groups[0].Items[0].$type : {result.Groups[0].Items[0]._type}"); // "Document"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 0) Debug.Log($"Groups[0].Items[0].DocumentIndex : {result.Groups[0].Items[0].DocumentIndex}"); // 0
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 0 && result.Groups[0].Items[0].Title != null) Debug.Log($"Groups[0].Items[0].Title : {result.Groups[0].Items[0].Title}"); // "Test.cs"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 1 && result.Groups[0].Items[1]._type != null) Debug.Log($"Groups[0].Items[1].$type : {result.Groups[0].Items[1]._type}"); // "Bookmark"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 1 && result.Groups[0].Items[1].Name != null) Debug.Log($"Groups[0].Items[1].Name : {result.Groups[0].Items[1].Name}"); // "ST:1:0:{12345678-1234-1234-1234-1234567890ab}"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 0 && result.Groups[0].Items[0]._type != null) Debug.Log($"Groups[0].Items[0].$type : {result.Groups[0].Items[0]._type}"); // "Document"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 0) Debug.Log($"Groups[0].Items[0].DocumentIndex : {result.Groups[0].Items[0].DocumentIndex}"); // 0
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 0 && result.Groups[0].Items[0].Title != null) Debug.Log($"Groups[0].Items[0].Title : {result.Groups[0].Items[0].Title}"); // "Test.cs"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 1 && result.Groups[0].Items[1]._type != null) Debug.Log($"Groups[0].Items[1].$type : {result.Groups[0].Items[1]._type}"); // "Bookmark"
+				if (result.Groups != null && result.Groups.Length > 0 && result.Groups[0].Items != null && result.Groups[0].Items.Length > 1 && result.Groups[0].Items[1].Name != null) Debug.Log($"Groups[0].Items[1].Name : {result.Groups[0].Items[1].Name}"); // "ST:1:0:{12345678-1234-1234-1234-1234567890ab}"
 			}
 			else
 			{
@@ -53,16 +103,5 @@ public class JsonDeserializationExample : MonoBehaviour
 		{
 			Debug.LogError($"Deserialization error: {e.Message}");
 		}
-
-		//transform.GetComponent<Anonymous>().TryGetFloat(out myProperty);
-		//or if that is not possible because it needs the label, something like
-		//transform.GetComponent<Anonymous>().TryGetFloat("myProperty", out myProperty);
-		//other functions like
-		//transform.GetComponent<Anonymous>().HasFloat("myProperty");
-		//or
-		//var myProperty = transform.GetComponent<Anonymous>().GetFloat("myProperty");
-		//transform.GetComponent<Anonymous>().GetFloat(out myProperty);
-		//or
-		//transform.GetComponent<Anonymous>().GetFloat("myProperty", out myProperty);
 	}
 }
