94367ffea0cb0358cf8d24465c440fb1119d0c62
acakebread <acakebread@gmail.com>
2025-09-04 09:53:18 +0100

    repaired exception in generated output

--- Assets/System/Scripts/Sereialisation/jsontocs_usagegenerator.cs
+++ Assets/System/Scripts/Sereialisation/jsontocs_usagegenerator.cs
@@ -29,7 +29,8 @@ public static class jsontocs_usagegenerator
 		sb.AppendLine($"        string jsonString = @\"{EscapeJson(jsonString)}\";");
 		sb.AppendLine("        try");
 		sb.AppendLine("        {");
-		sb.AppendLine("            var data = JsonTocs.FromJson<Dictionary<string, object>>(jsonString);");
+		string rootType = data is object[]? "object[]" : "Dictionary<string, object>";
+		sb.AppendLine($"            var data = JsonTocs.FromJson<{rootType}>(jsonString);");
 		sb.AppendLine("            if (data != null)");
 		sb.AppendLine("            {");
 
@@ -224,6 +225,233 @@ public static class jsontocs_usagegenerator
 		return name;
 	}
 }
+
+//using System;
+ //using System.Collections.Generic;
+ //using System.Linq;
+ //using System.Text;
+
+//public static class jsontocs_usagegenerator
+//{
+//	public static string GenerateDeserializationCode(object data, string jsonString)
+//	{
+//		var sb = new StringBuilder();
+
+//		// ----- Header -----
+//		sb.AppendLine("using System;");
+//		sb.AppendLine("using System.Collections.Generic;");
+//		sb.AppendLine("using System.Linq;");
+//		sb.AppendLine("using UnityEngine;");
+//		sb.AppendLine();
+//		sb.AppendLine("public class JsonDeserializationExample : MonoBehaviour");
+//		sb.AppendLine("{");
+//		sb.AppendLine("    static class J");
+//		sb.AppendLine("    {");
+//		sb.AppendLine("        public static Dictionary<string, object> D(object o) => o as Dictionary<string, object>;");
+//		sb.AppendLine("        public static object[] A(object o) => o as object[];");
+//		sb.AppendLine("        public static object G(Dictionary<string, object> d, string k) => (d != null && d.ContainsKey(k)) ? d[k] : null;");
+//		sb.AppendLine("    }");
+//		sb.AppendLine();
+//		sb.AppendLine("    void Start()");
+//		sb.AppendLine("    {");
+//		sb.AppendLine($"        string jsonString = @\"{EscapeJson(jsonString)}\";");
+//		sb.AppendLine("        try");
+//		sb.AppendLine("        {");
+//		sb.AppendLine("            var data = JsonTocs.FromJson<Dictionary<string, object>>(jsonString);");
+//		sb.AppendLine("            if (data != null)");
+//		sb.AppendLine("            {");
+
+//		// ----- TEMPLATE -----
+//		sb.Append("                var template = ");
+//		sb.Append(BuildTemplate(data));
+//		sb.AppendLine(";");
+
+//		// ----- RESULT -----
+//		sb.Append("                var result = ");
+//		sb.Append(BuildValue(data, "data", 0));
+//		sb.AppendLine(";");
+
+//		// ----- LOGS -----
+//		var logs = new StringBuilder();
+//		int uid = 0;
+//		GenerateLogs(logs, data, "result", "", 16, ref uid);
+//		sb.Append(logs.ToString());
+
+//		sb.AppendLine("            }");
+//		sb.AppendLine("            else");
+//		sb.AppendLine("            {");
+//		sb.AppendLine("                Debug.LogError(\"Failed to deserialize JSON\");");
+//		sb.AppendLine("            }");
+//		sb.AppendLine("        }");
+//		sb.AppendLine("        catch (System.Exception e)");
+//		sb.AppendLine("        {");
+//		sb.AppendLine("            Debug.LogError($\"Deserialization error: {e.Message}\");");
+//		sb.AppendLine("        }");
+//		sb.AppendLine("    }");
+//		sb.AppendLine("}");
+
+//		return sb.ToString();
+//	}
+
+//	// ---------------- TEMPLATE BUILDER ----------------
+//	private static string BuildTemplate(object node)
+//	{
+//		if (node is IDictionary<string, object> dict)
+//		{
+//			var used = new HashSet<string>(StringComparer.Ordinal);
+//			var props = new List<string>();
+//			foreach (var kv in dict.OrderBy(k => k.Key))
+//			{
+//				string name = "_" + UniqueSanitized(kv.Key, used);
+//				props.Add($"{name} = {BuildTemplate(kv.Value)}");
+//			}
+//			return $"new {{ {string.Join(", ", props)} }}";
+//		}
+//		if (node is object[] arr)
+//		{
+//			object sample = arr.FirstOrDefault();
+//			string elem = sample != null ? BuildTemplate(sample) : "(object)null";
+//			return $"new[] {{ {elem} }}";
+//		}
+//		return "(object)null";
+//	}
+
+//	// ---------------- VALUE BUILDER ----------------
+//	private static string BuildValue(object node, string expr, int depth)
+//	{
+//		if (node is IDictionary<string, object> dict)
+//		{
+//			var used = new HashSet<string>(StringComparer.Ordinal);
+//			var props = new List<string>();
+//			foreach (var kv in dict.OrderBy(k => k.Key))
+//			{
+//				string key = kv.Key;
+//				string safe = "_" + UniqueSanitized(key, used);
+//				string get = $"J.G({expr}, \"{EscapeStringLiteral(key)}\")";
+
+//				if (kv.Value is IDictionary<string, object>)
+//				{
+//					props.Add($"{safe} = {BuildValue(kv.Value, $"J.D({get})", depth + 1)}");
+//				}
+//				else if (kv.Value is object[] arr)
+//				{
+//					string item = $"item{depth}";
+//					string idx = $"i{depth}";
+//					object sample = arr.FirstOrDefault();
+//					string elemExpr = sample != null
+//						? BuildValue(sample, $"J.D({item})", depth + 1)
+//						: item;
+//					string defaultElem = sample != null
+//						? BuildTemplate(sample)
+//						: "(object)null";
+
+//					props.Add($"{safe} = (J.A({get}) != null ? J.A({get}).Select(({item}, {idx}) => {elemExpr}).ToArray() : new[] {{ {defaultElem} }})");
+//				}
+//				else
+//				{
+//					props.Add($"{safe} = {get}");
+//				}
+//			}
+//			return $"new {{ {string.Join(", ", props)} }}";
+//		}
+//		else if (node is object[] arr)
+//		{
+//			string item = $"item{depth}";
+//			string idx = $"i{depth}";
+//			object sample = arr.FirstOrDefault();
+//			string elemExpr = sample != null
+//				? BuildValue(sample, $"J.D({item})", depth + 1)
+//				: item;
+//			string defaultElem = sample != null
+//				? BuildTemplate(sample)
+//				: "(object)null";
+
+//			return $"(J.A({expr}) != null ? J.A({expr}).Select(({item}, {idx}) => {elemExpr}).ToArray() : new[] {{ {defaultElem} }})";
+//		}
+//		else
+//		{
+//			return expr;
+//		}
+//	}
+
+//	// ---------------- LOG BUILDER ----------------
+//	private static void GenerateLogs(StringBuilder sb, object node, string varExpr, string path, int indent, ref int uid)
+//	{
+//		string tabs = new string(' ', indent);
+
+//		if (node is IDictionary<string, object> dict)
+//		{
+//			foreach (var kv in dict.OrderBy(k => k.Key))
+//			{
+//				string safe = "_" + Sanitize(kv.Key);
+//				string child = $"{varExpr}.{safe}";
+//				string newPath = string.IsNullOrEmpty(path) ? kv.Key : path + "." + kv.Key;
+
+//				if (kv.Value is IDictionary<string, object> || kv.Value is object[])
+//				{
+//					GenerateLogs(sb, kv.Value, child, newPath, indent, ref uid);
+//				}
+//				else
+//				{
+//					sb.AppendLine($"{tabs}if ({child} != null) Debug.Log($\"{EscapePath(newPath)} : {{{child}}}\");");
+//				}
+//			}
+//		}
+//		else if (node is object[] arr)
+//		{
+//			sb.AppendLine($"{tabs}if ({varExpr} != null) Debug.Log($\"{EscapePath(path)} (array length): {{{varExpr}.Length}}\");");
+
+//			string idx = "i" + (uid++);
+//			sb.AppendLine($"{tabs}if ({varExpr} != null)");
+//			sb.AppendLine($"{tabs}{{");
+//			sb.AppendLine($"{tabs}    for (int {idx} = 0; {idx} < {varExpr}.Length; {idx}++)");
+//			sb.AppendLine($"{tabs}    {{");
+
+//			string elemExpr = $"{varExpr}[{idx}]";
+//			string elemPath = $"{path}[{{{idx}}}]";
+//			object sample = arr.FirstOrDefault();
+//			if (sample is IDictionary<string, object> || sample is object[])
+//			{
+//				GenerateLogs(sb, sample, elemExpr, elemPath, indent + 8, ref uid);
+//			}
+//			else
+//			{
+//				sb.AppendLine($"{tabs}        if ({elemExpr} != null) Debug.Log($\"{EscapePath(elemPath)} : {{{elemExpr}}}\");");
+//			}
+
+//			sb.AppendLine($"{tabs}    }}");
+//			sb.AppendLine($"{tabs}}}");
+//		}
+//	}
+
+//	// ---------------- Utilities ----------------
+//	private static string EscapeJson(string json) => (json ?? "").Replace("\"", "\"\"");
+//	private static string EscapeStringLiteral(string s) => (s ?? "").Replace("\\", "\\\\").Replace("\"", "\\\"");
+//	private static string EscapePath(string s) => (s ?? "").Replace("{", "{{").Replace("}", "}}");
+
+//	private static string Sanitize(string key)
+//	{
+//		if (string.IsNullOrEmpty(key)) return "_";
+//		var sb = new StringBuilder();
+//		bool first = true;
+//		foreach (char c in key)
+//		{
+//			if (first && char.IsDigit(c)) sb.Append('_');
+//			sb.Append(char.IsLetterOrDigit(c) ? c : '_');
+//			first = false;
+//		}
+//		return sb.ToString();
+//	}
+
+//	private static string UniqueSanitized(string key, HashSet<string> used)
+//	{
+//		string baseName = Sanitize(key);
+//		string name = baseName;
+//		int n = 1;
+//		while (!used.Add(name)) name = baseName + "_" + n++;
+//		return name;
+//	}
+//}
 
 
 
--- Assets/System/Tests/jsonExample/JsonDeserialisationExample.cs
+++ Assets/System/Tests/jsonExample/JsonDeserialisationExample.cs
@@ -14,23 +14,37 @@ public class JsonDeserializationExample : MonoBehaviour
 
 	void Start()
 	{
-		string jsonString = @"[{""$type"": ""Item"", ""id"": 1}, {""$type"": ""Item"", ""id"": 2}]";
+		string jsonString = @"{
+    ""team"": ""Alpha"",
+    ""members"": [
+        { ""name"": ""John"", ""roles"": [""Leader"", ""Developer""] },
+        { ""name"": ""Jane"", ""roles"": [""Tester""], ""details"": { ""level"": 3, ""active"": true } }
+    ]
+}";
 		try
 		{
 			var data = JsonTocs.FromJson<Dictionary<string, object>>(jsonString);
 			if (data != null)
 			{
-				var template = new[] { new { __type = (object)null, _id = (object)null } };
-				var result = (J.A(data) != null ? J.A(data).Select((item0, i0) => new { __type = J.G(J.D(item0), "$type"), _id = J.G(J.D(item0), "id") }).ToArray() : new[] { new { __type = (object)null, _id = (object)null } });
-				if (result != null) Debug.Log($" (array length): {result.Length}");
-				if (result != null)
+				var template = new { _members = new[] { new { _name = (object)null, _roles = new[] { (object)null } } }, _team = (object)null };
+				var result = new { _members = (J.A(J.G(data, "members")) != null ? J.A(J.G(data, "members")).Select((item0, i0) => new { _name = J.G(J.D(item0), "name"), _roles = (J.A(J.G(J.D(item0), "roles")) != null ? J.A(J.G(J.D(item0), "roles")).Select((item1, i1) => J.D(item1)).ToArray() : new[] { (object)null }) }).ToArray() : new[] { new { _name = (object)null, _roles = new[] { (object)null } } }), _team = J.G(data, "team") };
+				if (result._members != null) Debug.Log($"members (array length): {result._members.Length}");
+				if (result._members != null)
 				{
-					for (int i0 = 0; i0 < result.Length; i0++)
+					for (int i0 = 0; i0 < result._members.Length; i0++)
 					{
-						if (result[i0].__type != null) Debug.Log($"[{{i0}}].$type : {result[i0].__type}");
-						if (result[i0]._id != null) Debug.Log($"[{{i0}}].id : {result[i0]._id}");
+						if (result._members[i0]._name != null) Debug.Log($"members[{{i0}}].name : {result._members[i0]._name}");
+						if (result._members[i0]._roles != null) Debug.Log($"members[{{i0}}].roles (array length): {result._members[i0]._roles.Length}");
+						if (result._members[i0]._roles != null)
+						{
+							for (int i1 = 0; i1 < result._members[i0]._roles.Length; i1++)
+							{
+								if (result._members[i0]._roles[i1] != null) Debug.Log($"members[{{i0}}].roles[{{i1}}] : {result._members[i0]._roles[i1]}");
+							}
+						}
 					}
 				}
+				if (result._team != null) Debug.Log($"team : {result._team}");
 			}
 			else
 			{
