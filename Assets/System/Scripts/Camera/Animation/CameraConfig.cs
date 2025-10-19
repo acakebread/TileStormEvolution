ebfc357e5315633646ed931839e525e93ef2492e
acakebread <acakebread@gmail.com>
2025-10-19 10:42:27 +0100

    Update CameraConfig.cs

--- Assets/System/Scripts/Camera/Animation/CameraConfig.cs
+++ Assets/System/Scripts/Camera/Animation/CameraConfig.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MassiveHadronLtd
@@ -9,6 +8,5 @@ namespace MassiveHadronLtd
 		public CameraData data;
 		public Func<Vector3> origin;
 		public Func<Vector3> target;
-		//public Func<IReadOnlyList<Vector3>> points;
 	}
 }
