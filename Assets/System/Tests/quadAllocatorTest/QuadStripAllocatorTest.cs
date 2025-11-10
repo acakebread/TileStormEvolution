d3211d952b18e73aef06fa179e0c06c35e187cb7
acakebread <acakebread@gmail.com>
2025-11-10 11:04:08 +0000

    namespaced quad strip allocator

--- Assets/System/Tests/quadAllocatorTest/QuadStripAllocatorTest.cs
+++ Assets/System/Tests/quadAllocatorTest/QuadStripAllocatorTest.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using MassiveHadronLtd;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class QuadStripAllocatorTest : MonoBehaviour
--- Assets/System/Tests/quadAllocatorTest/QuadStripAllocatorTestGL.cs
+++ Assets/System/Tests/quadAllocatorTest/QuadStripAllocatorTestGL.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using MassiveHadronLtd;
 
 public class QuadStripAllocatorTestGL : MonoBehaviour
 {
