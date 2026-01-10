7422de7a73879e2d81b3287a7393f5780c902db3
acakebread <acakebread@gmail.com>
2026-01-10 16:09:35 +0000

    working panel system

--- Assets/Application/ClassicTS/Scripts/UISystem/UIPanel.cs
+++ Assets/Application/ClassicTS/Scripts/UISystem/UIPanel.cs
@@ -12,7 +12,7 @@ namespace ClassicTilestorm
 		[SerializeField] private bool resizable = true;
 		[SerializeField, Tooltip("Resize grab zone thickness in pixels")]
 		private float resizeBorder = 32f; // increased from 8f
-		[SerializeField] private Vector2 minSize = new Vector2(150, 100);
+		[SerializeField] private Vector2 minSize = new Vector2(256, 100);
 
 		private RectTransform rect;
 		private Canvas canvas;
