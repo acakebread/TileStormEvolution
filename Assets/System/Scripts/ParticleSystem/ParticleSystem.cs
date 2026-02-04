d07f96d512efca3e79e628344c70c6f5618baf54
acakebread <acakebread@gmail.com>
2026-02-04 19:52:25 +0000

    attempt to fix occasional exception in particle system render

--- Assets/System/Scripts/ParticleSystem/ParticleSystem.cs
+++ Assets/System/Scripts/ParticleSystem/ParticleSystem.cs
@@ -180,6 +180,7 @@ namespace MassiveHadronLtd
 			}
 			ReportViewRendered(view);
 			pm = particleMeshes[slot];
+			if (null == pm) return;
 			//Graphics.DrawMesh(pm.mesh, Matrix4x4.identity, material, 0, renderingCamera);
 			//int transparentFXLayer = LayerMask.NameToLayer("TransparentFX");
 			//Graphics.DrawMesh(pm.mesh, Matrix4x4.identity, material, transparentFXLayer, renderingCamera);
