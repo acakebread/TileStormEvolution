65b7a7ead931e9e6153cc50c187e8375cb108c5f
acakebread <acakebread@gmail.com>
2026-02-04 20:20:46 +0000

    Update EditorController.cs

--- Assets/Application/ClassicTS/Scripts/EditorController.cs
+++ Assets/Application/ClassicTS/Scripts/EditorController.cs
@@ -167,7 +167,8 @@ namespace ClassicTilestorm
 		// UI & Input Detection
 		// ===================================================================
 
-		public bool IsMouseOverGui() => new Rect(margin, panelYoffset + spacing, buttonWidth + 20f, buttonHeight * 9 + spacing * 9).Contains(new Vector3(Input.mousePosition.x, Screen.height - Input.mousePosition.y, Input.mousePosition.z));
+		private int numItems = 0;
+		public bool IsMouseOverGui() => new Rect(margin, panelYoffset + spacing, buttonWidth + 20f, (buttonHeight + spacing) * numItems).Contains(new Vector3(Input.mousePosition.x, Screen.height - Input.mousePosition.y, Input.mousePosition.z));
 
 		private void DrawMainUI(string mode, bool gridVisible)
 		{
@@ -200,22 +201,24 @@ namespace ClassicTilestorm
 
 			GUI.contentColor = prevContentColor;
 
+			numItems = ct;
+
 //			var mainController = GetComponent<MainController>();
 //#if UNITY_EDITOR
 //			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "Import Map", new Color(0.2f, 0.6f, 1f)))
 //				mainController.ImportMapAsAtomic();
 
-//			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "Export Map", new Color(0.8f, 0.2f, 0.2f)))
-//				mainController.ExportMapAsAtomic();
-//#endif
+			//			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "Export Map", new Color(0.8f, 0.2f, 0.2f)))
+			//				mainController.ExportMapAsAtomic();
+			//#endif
 
-//			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "(Re)Load Database", new Color(0.2f, 0.6f, 1f)))
-//				mainController.LoadDatabase();
+			//			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "(Re)Load Database", new Color(0.2f, 0.6f, 1f)))
+			//				mainController.LoadDatabase();
 
-//#if UNITY_EDITOR
-//			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "Save Database", new Color(0.8f, 0.2f, 0.2f)))
-//				mainController.SaveDatabase();
-//#endif
+			//#if UNITY_EDITOR
+			//			if (GuiUtils.ColoredButton(new Rect(margin, y + ct++ * (buttonHeight + spacing), buttonWidth, buttonHeight), "Save Database", new Color(0.8f, 0.2f, 0.2f)))
+			//				mainController.SaveDatabase();
+			//#endif
 		}
 	}
 }
