65c6ad5bf7968c0447b7bb81fda3f7ae3323e9cb
acakebread <acakebread@gmail.com>
2026-05-14 14:07:08 +0100

    commented version of runtime browser system - not required for tests

--- Assets/System/Tests/assetImporterTest/AssetImporterTest.cs
+++ Assets/System/Tests/assetImporterTest/AssetImporterTest.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using UnityEngine;
 
 namespace MassiveHadronLtd
@@ -70,3 +70,72 @@ namespace MassiveHadronLtd
 		}
 	}
 }
+
+
+//using System.IO;
+//using MassiveHadronLtd.FileBrowserUtil;
+//using UnityEngine;
+
+//namespace MassiveHadronLtd
+//{
+//	public class AssetImporterTest : MonoBehaviour
+//	{
+//		[Header("Asset Importer Test")]
+//		[Tooltip("Last successfully imported model folder")]
+//		public string lastImportedPath;
+//		[Tooltip("How imported Wavefront assets are organized on disk")]
+//		public AssetImporter.ImportOption importOption = AssetImporter.ImportOption.HashIdFolder;
+
+//		private void OnGUI()
+//		{
+//			GUILayout.BeginArea(new Rect(20, 20, 400, 300));
+//			GUILayout.Label("=== Asset Importer Test ===", GUILayout.Height(30));
+
+//			if (GUILayout.Button("Import .OBJ + Dependencies", GUILayout.Height(50)))
+//			{
+//				ImportObjFile();
+//			}
+
+//			if (!string.IsNullOrEmpty(lastImportedPath))
+//			{
+//				GUILayout.Space(10);
+//				GUILayout.Label("Last Imported:", GUILayout.Height(20));
+//				GUILayout.Label(lastImportedPath, GUILayout.Height(40));
+
+//				if (GUILayout.Button("Open Imported Folder", GUILayout.Height(30)))
+//				{
+//					OpenImportedFolder();
+//				}
+//			}
+
+//			GUILayout.EndArea();
+//		}
+
+//		public void ImportObjFile()
+//		{
+//			RuntimeFileBrowser.OpenObjFile(
+//				"Select Wavefront .obj file",
+//				path =>
+//				{
+//					lastImportedPath = AssetImporter.ImportWavefrontModel(path, importOption);
+
+//					if (!string.IsNullOrEmpty(lastImportedPath))
+//					{
+//						Debug.Log($"✅ Import successful!\nPath: {lastImportedPath}");
+//					}
+//				},
+//				RuntimeFileBrowser.GetDefaultRootFolder());
+//		}
+
+//		private void OpenImportedFolder()
+//		{
+//#if UNITY_EDITOR
+//			if (!string.IsNullOrEmpty(lastImportedPath))
+//			{
+//				string folder = Path.GetDirectoryName(lastImportedPath);
+//				UnityEditor.EditorUtility.RevealInFinder(folder);
+//			}
+//#endif
+//		}
+//	}
+//}
