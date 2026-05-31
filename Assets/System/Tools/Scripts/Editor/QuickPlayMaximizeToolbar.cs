dcabd638e2c2e5323fcaa9d2e043086062bba855
acakebread <acakebread@gmail.com>
2026-05-31 07:40:26 +0100

    added a maximise game view keyboard and menu shortcut

---
+++ Assets/System/Tools/Scripts/Editor/QuickPlayMaximizeToolbar.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace MassiveHadronLtd.EditorTools
+{
+	public static class QuickPlayMaximizeToolbar
+	{
+		private static readonly Type GameViewType = Type.GetType("UnityEditor.GameView, UnityEditor");
+
+		[MenuItem("Window/MassiveHadron/Toggle Max Play %#m", false, 2000)]
+		private static void ToggleMaxPlay()
+		{
+			var gameView = FindGameView();
+			if (gameView == null)
+			{
+				Debug.LogWarning("QuickPlayMaximize: Could not find the Game view.");
+				return;
+			}
+
+			var targetState = !GetMaximizeOnPlay(gameView);
+			SetMaximizeOnPlay(gameView, targetState);
+			EditorApplication.ExecuteMenuItem("Window/General/Game");
+			gameView.maximized = targetState;
+			gameView.Focus();
+		}
+
+		[MenuItem("Window/MassiveHadron/Toggle Max Play %#m", true)]
+		private static bool ToggleMaxPlayValidate() => true;
+
+		private static EditorWindow FindGameView()
+		{
+			if (GameViewType == null)
+				return null;
+
+			return Resources.FindObjectsOfTypeAll(GameViewType)
+				.OfType<EditorWindow>()
+				.FirstOrDefault();
+		}
+
+		private static bool GetMaximizeOnPlay(EditorWindow gameView)
+		{
+			if (gameView == null)
+				return false;
+
+			var type = gameView.GetType();
+			var property = type.GetProperty("maximizeOnPlay", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
+			if (property != null && property.PropertyType == typeof(bool))
+				return (bool)property.GetValue(gameView);
+
+			var field = type.GetField("maximizeOnPlay", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)
+					 ?? type.GetField("m_MaximizeOnPlay", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
+			if (field != null && field.FieldType == typeof(bool))
+				return (bool)field.GetValue(gameView);
+
+			return false;
+		}
+
+		private static void SetMaximizeOnPlay(EditorWindow gameView, bool value)
+		{
+			if (gameView == null)
+				return;
+
+			var type = gameView.GetType();
+			var property = type.GetProperty("maximizeOnPlay", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
+			if (property != null && property.PropertyType == typeof(bool) && property.CanWrite)
+			{
+				property.SetValue(gameView, value);
+				EditorUtility.SetDirty(gameView);
+				return;
+			}
+
+			var field = type.GetField("maximizeOnPlay", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)
+					 ?? type.GetField("m_MaximizeOnPlay", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
+			if (field != null && field.FieldType == typeof(bool))
+			{
+				field.SetValue(gameView, value);
+				EditorUtility.SetDirty(gameView);
+			}
+		}
+	}
+}
